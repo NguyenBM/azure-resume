2f28a9db4f85baf3be47a7142252e6b78b50fa70
Brian Nguyen <nguyenbrian@gmail.com>
2024-05-26 21:33:47 -0400

    Made changes to backend

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -22,6 +22,8 @@ namespace Company.Function
             [CosmosDB(databaseName: "AzureResume", containerName: "Counter", Connection="AzureResumeConnectionString", Id="1", PartitionKey="1")] out Counter updatedCounter,
             ILogger log)
         {
+
+            // Updates counter right here
             log.LogInformation("C# HTTP trigger function processed a request.");
             
             updatedCounter = counter;
